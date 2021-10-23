f4264f45b783cb85f1b1aeb93710b24606986e10
giacoletti <giacoletti92@gmail.com>
2021-10-23 20:16:54 +0200

    Writing first test to test that a number will be returned when a number is entered, and creating method for fizz_buzz.rb

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,5 +1,9 @@
 require './lib/fizz_buzz'
 
-describe "fizz_buzz" do
+describe "fizz_buzz" do  #"fizz_buzz" must match our "fizz_buzz.rb" file name?
     
+    it "returns 1 if number is 1" do
+        expect(fizz_buzz(1)).to eq 1  #it calls the method "fizz_buzz(1)" with 1 as argument and the output must be "eq 1", equal to 1.
+    end
+
 end
